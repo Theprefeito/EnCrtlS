413fb6cf023c998c1b0456a2b8dc4f966de962bd
IcaroL-byte <icaro.lauan@escolar.ifrn.edu.br>
2025-06-05 09:55:59 -0300

    Wall Sliding só para direita
    o wall slide funciona, porém só com paredes à direita

---
+++ EnCrtlS/Assets/Scripts/WallJump.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+
+public class WallJump : MonoBehaviour
+{
+    [SerializeField] Transform wallCheck;
+    [SerializeField] float wallCheckDistance;
+    [SerializeField] float wallSlideSpeed;
+    [SerializeField] LayerMask groundLayer;
+    private PlayerMovement player;
+    private Rigidbody2D rigPlayer;
+    public bool isFacingRight;
+    public bool onWall;
+    public bool wallSlide;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigPlayer = GetComponent<Rigidbody2D>();
+        player = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckWallNextTo();
+        CheckWallSlide();
+        Flip();
+    }
+
+    void CheckWallNextTo()
+    {
+        onWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, groundLayer);
+    }
+
+    private void CheckWallSlide()
+    {
+        if (onWall && player.inFloor == false && rigPlayer.linearVelocity.y < 0 && Input.GetAxis("Horizontal") != 0f)
+        {
+            wallSlide = true;
+        }
+
+        else
+        {
+            wallSlide = false;
+        }
+   
+        if(wallSlide)
+        {
+            if(rigPlayer.linearVelocity.y < -wallSlideSpeed)
+            {
+                rigPlayer.linearVelocityY = -wallSlideSpeed;
+            }
+        }
+    }
+
+    void Flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            if (isFacingRight)
+            {
+                Vector3 attackPos = wallCheck.localPosition;
+                attackPos.x *= -1;
+                wallCheck.localPosition = attackPos;
+
+                isFacingRight = false;
+            }
+        }
+
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            if (!isFacingRight)
+            {
+                Vector3 attackPos = wallCheck.localPosition;
+                attackPos.x *= -1;
+                wallCheck.localPosition = attackPos;
+
+                isFacingRight = true;
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        
+        if (!isFacingRight)
+        {
+            Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
+        }
+
+        else
+        {
+            Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x - wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
+        }
+        
+    }
+}
