ec6ca082c0328bb2f0d01ece1b12317717a2c1a3
IcaroL-byte <icaro.lauan@escolar.ifrn.edu.br>
2025-09-20 10:50:48 -0300

    Corrigindo o bug da camera(eu acho)

--- EnCrtlS/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ EnCrtlS/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -317,7 +317,7 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Room"))
         {
