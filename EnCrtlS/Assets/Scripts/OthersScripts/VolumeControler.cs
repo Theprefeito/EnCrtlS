c12b8d4d1ffb08993ead42ac3aa5493d7304c4e9
FernandoSantos0920 <fernando.santos2@escolar.ifrn.edu.br>
2025-10-29 15:55:04 -0300

    Volume funcional

---
+++ EnCrtlS/Assets/Scripts/OthersScripts/VolumeControler.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class AudioControler : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public float volume = 0;
+
+    public Slider slider;
+
+    public TMP_Text texto;
+
+
+    void Start()
+    {
+        audioMixer.GetFloat("Master", out volume);
+        slider.value = volume;
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            volume += 1;
+            audioMixer.SetFloat("Master", volume);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) && volume > -80)
+        {
+            volume -= 1;
+            audioMixer.SetFloat("Master", volume);
+        }
+
+        volume = slider.value;
+
+        texto.text = volume.ToString();
+
+        if (volume <= -40)
+        {
+            audioMixer.SetFloat("Master", -80);
+        }
+        else
+        {
+            audioMixer.SetFloat("Master", volume);
+
+        }
+
+
+    }
+}
